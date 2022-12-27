594bf2f9112256bbf95a12cb3ac0072313edf669
ayah05 <84806479+ayah05@users.noreply.github.com>
2022-12-27 12:27:10 +0100

    Initial commit

---
+++ dataInputs.php
@@ -0,0 +1,98 @@
+<?php include ("validation.php")?>
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset ="UTF -8">
+    <meta name =" viewport " content =" width = device-width , initial - scale =1.0 ">
+    <title> My PHP Web Application </title >
+    <link rel="stylesheet" href="websiteStyle.css">
+</head>
+<body>
+<style>
+    .required:before{
+        content:"*";
+        color:red;
+    }
+</style>
+<h1 align="center"> Patient Administration </h1>
+<form method="POST" action="">
+    <label for="patientID" class="required">Patient ID: </label>
+    <input type="number" name="patientID" id="patientID">
+    <span class="errorMsg"> <?php if (isset($patientID_ERROR)) {echo $patientID_ERROR;}?></span>
+    <span class="successMsg"> <?php if (isset($patientID_SUCCESS)) {echo $patientID_SUCCESS;}?></span><br><br>
+
+    <label for="firstname" class="required">Firstname: </label>
+    <input type="text" name="firstname" id="firstname">
+    <span class="errorMsg"> <?php if (isset($firstname_ERROR)) {echo $firstname_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($firstname_SUCCESS)) {echo $firstname_SUCCESS;}?></span><br><br>
+
+    Second Name: <input type="text" name="secondName">
+    <span class="successMsg"> <?php if (isset($secondName_SUCCESS)) {echo $secondName_SUCCESS;}?></span><br><br>
+    <label for="familyName" class="required">Family Name: </label>
+    <input type="text" name="familyName" id="familyName">
+    <span class="errorMsg"> <?php if (isset($familyName_ERROR)) {echo $familyName_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($familyName_SUCCESS)) {echo $familyName_SUCCESS;}?></span><br><br>
+
+    <label for="birthdate" class="required">Birthday:  </label>
+    <input type="date" name="birthdate" id="birthdate">
+    <span class="errorMsg"> <?php if (isset($birthdate_ERROR)) {echo $birthdate_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($birthdate_SUCCESS)) {echo $birthdate_SUCCESS;}?></span><br><br>
+
+    <label for="gender" class="required">Gender: </label>
+    <input type="radio" name="gender" value="female" id="gender">Female <input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="other">Other<br><br>
+    <span class="errorMsg"> <?php if (isset($gender_ERROR)) {echo $gender_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($gender_SUCCESS)) {echo $gender_SUCCESS;}?></span><br><br>
+
+    <label for="maritalStatus" class="required">Marital Status:  </label>
+    <input type="radio" name="maritalStatus" value="single" id="maritalStatus">Single <input type="radio" name="maritalStatus" value="married">Married <input type="radio" name="maritalStatus" value="divorced">Divorced  <input type="radio" name="maritalStatus" value="domesticPartner">Domestic Partner<br><br>
+    <span class="errorMsg"> <?php if (isset($maritalStatus_ERROR)) {echo $maritalStatus_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($maritalStatus_SUCCESS)) {echo $maritalStatus_SUCCESS;}?></span><br><br>
+
+    <label for="address" class="required">Address:  </label>
+    <input type="text" name="address" id="address">
+    <span class="errorMsg"> <?php if (isset($address_ERROR)) {echo $address_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($address_SUCCESS)) {echo $address_SUCCESS;}?></span><br><br>
+
+    <label for="postalCode" class="required">Postal Code: </label>
+    <input type="number" name="postalCode" id="postalCode">
+    <span class="errorMsg"> <?php if (isset($postalCode_ERROR)) {echo $postalCode_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($postalCode_SUCCESS)) {echo $postalCode_SUCCESS;}?></span><br><br>
+
+    <label for="city" class="required">City: </label>
+    <input type="text" name="city" id="city">
+    <span class="errorMsg"> <?php if (isset($city_ERROR)) {echo $city_ERROR; }?></span>
+    <span class="successMsg"> <?php if (isset($city_SUCCESS)) {echo $city_SUCCESS;}?></span><br><br>
+
+    <label for="phoneNumber" class="required">Phone Number: + </label>
+    <input type="number" name="phoneNumber" id="phoneNumber">
+    <span class="errorMsg"> <?php if (isset($phoneNumber_ERROR)) {echo $phoneNumber_ERROR;} ?></span>
+    <span class="successMsg"> <?php if (isset($phoneNumber_SUCCESS)) {echo $phoneNumber_SUCCESS;}?></span><br><br>
+
+    <label for="email" class="required">E-Mail: </label>
+    <input type="email" name="email" id="email">
+    <span class="errorMsg"> <?php if (isset($email_ERROR)) {echo $email_ERROR; }?></span>
+    <span class="successMsg"> <?php if (isset($email_SUCCESS)) {echo $email_SUCCESS;}?></span><br><br>
+
+    <label for="weekDays" class="required"> Week Days: </label>
+    <input type="checkbox" name="weekDays[]" value="Monday" id="weekDays"> Monday
+    <input type="checkbox" name="weekDays[]" value="Tuesday" id="weekDays"> Tuesday
+    <input type="checkbox" name="weekDays[]" value="Wednesday" id="weekDays"> Wednesday
+    <input type="checkbox" name="weekDays[]" value="Thursday" id="weekDays"> Thursday
+    <input type="checkbox" name="weekDays[]" value="Friday" id="weekDays"> Friday
+    <input type="checkbox" name="weekDays[]" value="Saturday" id="weekDays"> Saturday
+    <input type="checkbox" name="weekDays[]" value="Sunday" id="weekDays"> Sunday <br>
+    <span class="successMsg"> <?php if (isset($weekdays_SUCCESS)) {echo $weekdays_SUCCESS;}?></span>
+    <span class="errorMsg"> <?php if (isset($weekdays_ERROR)) {echo $weekdays_ERROR; }?></span><br><br>
+
+
+    <p></p>
+
+
+    <button type="submit" class="submit" name="sendDataButton" align="center"> Send Data</button><br>
+    <p></p>
+
+    <span style="font-size:25px;color:darkgreen"> <?php if (isset($impt)) {echo "The generated patient's ID in the FHIR resource is: ";echo $impt;}?></span>
+
+</form>
+</body>
+</html>
---
+++ validation.php
@@ -0,0 +1,241 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $dateNow = date("Y-m-d");
+    $dontproceed=0;
+    if (empty($_POST["patientID"])) {
+        $patientID_ERROR = 'Please enter the Patient-ID!';
+    }else {
+        if (strlen($_POST["patientID"]) == 5) {
+            //echo "Patient ID:",$_POST["patientID"];echo "<br>";
+            $patientID_SUCCESS = "PatientID set successfully";
+            $dontproceed=0;
+        }else{
+            $patientID_ERROR = 'Invalid Patient-ID!';
+            $dontproceed=1;
+        }
+    }
+
+    if (empty($_POST["firstname"])) {
+        $firstname_ERROR = 'Please enter your Firstname!';
+    }else {
+        //echo "Firstname: ",$_POST["firstname"];echo "<br>";
+        $firstname_SUCCESS = "First Name set successfully";
+    }
+
+    if (!(empty($_POST["secondName"]))){
+        //echo "Second Name: ",$_POST["secondName"];echo "<br>";
+        $secondName_SUCCESS = "Second Name set successfully";
+    }
+
+    if (empty($_POST["familyName"])) {
+        $familyName_ERROR = 'Please enter your Family Name!';
+    }else {
+        //echo "Family Name: ",$_POST["familyName"];echo "<br>";
+        $familyName_SUCCESS = "Family Name set successfully";
+    }
+
+    if (empty($_POST["birthdate"])){
+        $birthdate_ERROR = "Please enter your Birthdate!";
+    }else {
+        if ($_POST["birthdate"] < $dateNow){
+            //echo "Birthdate: ",$_POST["birthdate"];echo "<br>";
+            $birthdate_SUCCESS = "Birthdate set successfully.";
+            $dontproceed=0;
+        }else if($_POST["birthdate"] > $dateNow){
+            $birthdate_ERROR = "Invalid Birthdate. Date exceeds today's Date.";
+            $dontproceed=1;
+        }
+    }
+
+    if(empty($_POST["gender"])){
+        $gender_ERROR = "Please select a Gender!";
+    }else {
+        //echo "Gender: ";
+        if($_POST["gender"] == "female"){
+            // echo "Female";echo "<br>";
+            $gender_SUCCESS = "Gender set successfully.";
+        } if($_POST["gender"] == "male"){
+            // echo "Male";echo "<br>";
+            $gender_SUCCESS="Gender set successfully.";
+        }if ($_POST["gender"] == "other") {
+            // echo "Other";echo "<br>";
+            $gender_SUCCESS="Gender set successfully.";
+        }}
+
+
+    if(empty($_POST["maritalStatus"])){
+        $maritalStatus_ERROR = "Please select a Marital Status!";
+    }else {
+        // echo "Marital Status: ";
+        if($_POST["maritalStatus"] == "single"){
+            // echo "Single";echo "<br>";
+            $maritalStatus_SUCCESS = "Marital Status set successfully.";
+        }if($_POST["maritalStatus"] == "married"){
+            // echo "Married";echo "<br>";
+            $maritalStatus_SUCCESS = "Marital Status set successfully.";
+        }if($_POST["maritalStatus"] == "divorced"){
+            // echo "Divorced";echo "<br>";
+            $maritalStatus_SUCCESS = "Marital Status set successfully.";
+        }if($_POST["maritalStatus"] == "domesticPartner"){
+            // echo "Domestic Partner";echo "<br>";
+            $maritalStatus_SUCCESS = "Marital Status set successfully.";
+        }
+    }
+
+    if (empty($_POST["address"])) {
+        $address_ERROR = 'Please enter your Address!';
+    }else {
+        // echo "Address: ",$_POST["address"];echo "<br>";
+        $address_SUCCESS = "Address set successfully.";
+    }
+
+    if (empty($_POST["email"])) {
+        $email_ERROR = 'Please enter your E-Mail address!';
+    }else {
+        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+            // echo "E-Mail: ",$_POST["email"]; echo "<br>";
+            $email_SUCCESS = "E-Mail set successfully.";
+            $dontproceed=0;
+        }else{
+            $email_ERROR = "Invalid E-Mail!";
+            $dontproceed=1;
+        }
+    }
+
+    if (empty($_POST["postalCode"])) {
+        $postalCode_ERROR = 'Please enter your Postal Code!';
+    }else {
+        if (strlen($_POST["postalCode"]) == 4) {
+            //echo "Postal Code: ",$_POST["postalCode"];echo "<br>";
+            $postalCode_SUCCESS = "Postal Code set successfully.";
+            $dontproceed=0;
+        } else{
+            $postalCode_ERROR = "Invalid Postal Code!";
+            $dontproceed=1;
+        }
+    }
+
+    if (empty($_POST["city"])) {
+        $city_ERROR = 'Please enter your City!';
+    }else {
+        //echo "City: ",$_POST["city"];echo "<br>";
+        $city_SUCCESS = "City set successfully.";
+    }
+
+    if (empty($_POST["phoneNumber"])) {
+        $phoneNumber_ERROR = 'Please enter your Phone Number!';
+    }else {
+        if (strlen($_POST["phoneNumber"])==11||strlen($_POST["phoneNumber"])==13){
+            //echo "Phone Number: ", $_POST["phoneNumber"];echo "<br>";
+            $phoneNumber_SUCCESS = "Phone Number set successfully.";
+            $dontproceed=0;
+        }else{
+            $phoneNumber_ERROR = "Invalid Phone Number!";
+            $dontproceed=1;
+        }
+    }
+
+
+    if (empty($_POST["weekDays"])){
+        $weekdays_ERROR = "Please select the days on which the patient should take the medication";
+        $dontproceed=1;
+    }else{
+        foreach ($_POST["weekDays"] as $value){
+            echo $value.'<br/>';
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+        }
+        /*if($_POST["weekDays"] == "Monday"){
+            echo "Monday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        } if($_POST["weekDays"] == "Tuesday"){
+            echo "Tuesday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        } if($_POST["weekDays"] == "Wednesday"){
+            echo "Wednesday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        }
+        if($_POST["weekDays"] == "Thursday"){
+            echo "Thursday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        } if($_POST["weekDays"] == "Friday"){
+            echo "Friday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        } if($_POST["weekDays"] == "Saturday"){
+            echo "Saturday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        } if($_POST["weekDays"] == "Sunday"){
+            echo "Sunday: ";echo "<br>";
+            $weekdays_SUCCESS = "The correct weekdays were set successfully";
+            $dontproceed=0;
+        }*/
+    }
+}
+
+if ((isset($_POST["patientID"]))&&(isset($_POST["firstname"]))&&(isset($_POST["familyName"]))&&(isset($_POST["birthdate"]))&&(isset($_POST["gender"]))&&(isset($_POST["maritalStatus"]))&&(isset($_POST["address"]))&&(isset($_POST["postalCode"]))&&(isset($_POST["city"]))&&(isset($_POST["phoneNumber"]))&&(isset($_POST["email"]))&&($dontproceed==0)){
+    $patient=array();
+    //Transform to JSON the required fields, as long as they are all fulfilled and the optional fields dont have wrong data.
+    $patient['resourceType']='Patient';
+    $patient['identifier']['id']=$_POST["patientID"];
+    $patient['name']['given']=$_POST["firstname"];
+    $patient['name']['family']=$_POST["familyName"];
+    $patient['birthDate']=$_POST["birthdate"];
+    $patient['gender']=$_POST["gender"];
+    $patient['maritalStatus']['coding']['display']=$_POST["maritalStatus"];
+    $patient['address']['line']=[$_POST["address"]];
+    $patient['address']['postalCode']=$_POST["postalCode"];
+    $patient['address']['city']=$_POST["city"];
+    $patient['telecom']['system']="phone";
+    $patient['telecom']['value']=$_POST["phoneNumber"];
+    $patient['telecom']['system']=["email"];
+    $patient['telecom']['value']=$_POST["email"];
+
+    //Transform to JSON the optional field in case it is set
+    if (isset($_POST["secondName"])){
+        $patient['name']['given']=[$_POST["firstname"], $_POST["secondName"]];
+    }
+
+    $patientJsonData=json_encode($patient);
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://hapi.fhir.org/baseR4/Patient',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $patientJsonData,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+
+    /* $delim=', "id"';
+    $pos=strpos($response,$delim);
+    echo $pos;
+    $important=substr($response,$pos+strlen($delim)-1,5);
+    echo $important; */
+
+    $impt=substr($response,40,7);
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+}
+
+?>
